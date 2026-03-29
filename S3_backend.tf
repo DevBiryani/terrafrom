b77c068562375c29ebff50110c05bf2131d3fb64
Aryan Singh <aryan@example.com>
2026-03-29 22:47:22 +0530

    Add S3 backend configuration with profile support

---
+++ S3_backend.tf
@@ -0,0 +1,8 @@
+# terraform {
+#   backend "s3" {
+#     bucket  = "my-backend-bucket"
+#     key     = "prod/terraform.tfstate"
+#     region  = "ap-south-1"
+#     profile = "backend-profile"
+#   }
+# }
--- provier.tf
+++ provier.tf
@@ -1,17 +1,25 @@
 terraform {
+     backend "s3" {
+    bucket  = "s3-backend-bucket-aryan-singh-2025" # Name of the S3 bucket to store the state file
+    key     = "Devlopment/terraform.tfstate" # Path within the bucket to store the state file
+    region  = "ap-south-1" # Mumbai region
+    profile = "backend-profile" # Make sure to set up this profile in your AWS CLI configuration
+    encrypt = true # Enable encryption for the state file
+    use_lockfile = true # Use lock file for state locking
+    lock_table = "s3-backend-lock-table-aryan-singh-2025" # DynamoDB table for state locking
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
-
-    # Adding random provider for generating random values for resources
     random = {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
   }
 }
+
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1" # Mumbai region

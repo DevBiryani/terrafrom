938b9cea90e16fad2136a946cc9f5236b9b12054
Aryan Singh <aryan@example.com>
2026-03-29 21:45:07 +0530

    Update Terraform configuration - provier.tf modifications

--- provier.tf
+++ provier.tf
@@ -4,9 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+
+    # Adding random provider for generating random values for resources
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 }
-
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1" # Mumbai region
